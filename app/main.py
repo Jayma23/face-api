f36079cb1d74b9ad6adbc57e0584c17be9d1c6e6
Jayma23 <146985526+Jayma23@users.noreply.github.com>
2025-07-25 12:36:40 -0700

    Update main.py, 添加绝对路径

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@
 
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
-from face_utils import extract_main_face_embedding
+from .face_utils import extract_main_face_embedding
 
 app = FastAPI()
 
